009549f27f7f59d16c28a67a5ed807d8bf15eeee
Mahima Hemachandra <nomad.inof@gmail.com>
2021-10-08 12:23:26 +0530

    Add files via upload

---
+++ python_Phonebook/main.py
@@ -0,0 +1,38 @@
+file_name = "C:\Users\Mahima\Documents\Phonebook.txt"
+file1 = open(file_name, "a+")
+file1.close
+
+
+def show_main_menu():
+    ''' Show main menu for Phone Book Program '''
+    print("\n   *** Phone Book Menu ***\n" +
+          "------------------------------------------\n" +
+          "Enter 1,2,3 or 4:\n" +
+          "Enter 1 To Display Your Contacts\n" +
+          "Enter 2 To Add a New Contact\n" +
+          "Enter 3 To search contacts\n" +
+          "Enter 4 To Exit\n**********************")
+
+    '''Operation input'''
+
+
+''' Search contacts'''
+def search_contact_record():
+    ''' This function is used to searches a specific contact record '''
+
+
+'''first name input'''
+def input_fname():
+    ''' Converts first letter of your name  to Upper case '''
+
+
+'''last name input'''
+def input_lname():
+    ''' Converts first letter of  last name  to Upper case '''
+
+
+'''insert contact'''
+def enter_contact_record():
+    ''' It  collects contact info firstname, last name, email and phone '''
+
+    
